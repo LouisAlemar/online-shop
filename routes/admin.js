c3f3579c9cca1eb4e42c8a03e10aee3c85c098d0
Louis Alemar <louisalemar@gmail.com>
2023-11-02 16:48:16 -0400

    delete products from cart

--- routes/admin.js
+++ routes/admin.js
@@ -14,4 +14,11 @@ router.get("/products", adminController.getProducts);
 // /admin/add-product => POST
 router.post("/add-product", adminController.postAddProduct);
 
+// /admin/edit-product/:productID => GET
+router.get("/edit-product/:productId", adminController.getEditProduct);
+
+router.post("/edit-product", adminController.postEditProduct);
+
+router.post("/delete-product", adminController.postDeleteProduct);
+
 module.exports = router;
